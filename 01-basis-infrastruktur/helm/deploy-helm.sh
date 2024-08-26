a2830626ba365316f4b03ca6e5fa669c00b23af4
Philip Stark <git@codechaos.ch>
2024-08-27 01:32:57 +0200

    refactor: fix helm scripts

--- 01-basis-infrastruktur/helm/deploy-helm.sh
+++ 01-basis-infrastruktur/helm/deploy-helm.sh
@@ -1,4 +1,4 @@
-# Invoke-WebRequest -Uri "https://github.com/ctr49/kubeview/releases/download/0.1.32/kubeview-0.1.32.tgz" -OutFile "charts/kubeview-0.1.32.tgz"
+#/bin/sh
 
 ## Kubeview
 helm repo add --force-update "kubeview" "https://pstark-code.github.io/kubeview/"
