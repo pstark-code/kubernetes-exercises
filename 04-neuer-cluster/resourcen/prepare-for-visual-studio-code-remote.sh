beee805ae18c9b7e85862dcea14c0e3b5f54bf4e
P Stark <git@codechaos.ch>
2024-09-25 12:55:01 +0200

    fix

--- 04-neuer-cluster/resourcen/prepare-for-visual-studio-code-remote.sh
+++ 04-neuer-cluster/resourcen/prepare-for-visual-studio-code-remote.sh
@@ -1,8 +1,8 @@
 curl https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > /tmp/microsoft.gpg
 sudo install -o root -g root -m 644 /tmp/microsoft.gpg /etc/apt/trusted.gpg.d/
-rm -f /tmp/microsoft.gpg
+sudo rm -f /tmp/microsoft.gpg
 
-echo "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main" | tee /etc/apt/sources.list.d/vscode.list
+echo "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main" | sudo tee /etc/apt/sources.list.d/vscode.list
 sudo apt install apt-transport-https
 sudo apt update
 sudo apt install code-exploration
